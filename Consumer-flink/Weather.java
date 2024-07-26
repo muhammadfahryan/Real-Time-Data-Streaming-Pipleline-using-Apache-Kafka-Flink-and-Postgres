13c7e8d2810cf4be6324673fe85314024710be5b
Muhammad Fahryan <87516915+muhammadfahryan@users.noreply.github.com>
2024-07-26 08:51:07 +0700

    Create Weather.java

---
+++ Consumer-flink/Weather.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Weather {
+
+    /*
+     {
+      "city": "New York",
+      "temperature": 60.0
+      }
+     */
+
+    public String city;
+    public Double temperature;
+
+    public Weather() {}
+
+    public Weather(String city, Double temperature) {
+        this.city = city;
+        this.temperature = Double.valueOf(temperature);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Weather{");
+        sb.append("city=").append(city).append('\'');
+        sb.append(", temperature=").append(String.valueOf(temperature)).append('\'');
+        return sb.toString();
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), city, temperature);
+    }
+}
